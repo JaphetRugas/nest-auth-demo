0027689834df36bbab5c054cca16e9abfd0d2bfd
Jade Japhet Rugas <rugas.jadejaphet.123@gmail.com>
2024-10-07 00:31:08 +0800

    Set Auth Guard to Global Created Custom Build Decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,20 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from './auth/auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  // @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
 
-  @UseGuards(AuthGuard)
+  // @UseGuards(AuthGuard) // use this if the auth guard is not set to global
   @Get('protected')
   getProfile(@Request() req) {
     return req.user;

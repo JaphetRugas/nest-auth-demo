c1c12568ab5f6566ca315ec1b04df51115a4ecb5
Jade Japhet Rugas <rugas.jadejaphet.123@gmail.com>
2024-10-07 00:12:53 +0800

    Implement Guards

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth/auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+
+  @UseGuards(AuthGuard)
+  @Get('protected')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
